18ec45c27ac57f008094c8f09bb65b517e04282f
Waleed Malik <zenalytiks@gmail.com>
2025-02-19 03:38:53 +0500

    adjust image resolution

--- src/lib/components/DashECharts.react.js
+++ src/lib/components/DashECharts.react.js
@@ -259,10 +259,14 @@ function DashECharts(props)  {
     const downloadChartAsPDF = async () => {
         if (!chartRef.current) return;
         
-        const canvas = await html2canvas(chartRef.current);
+        const canvas = await html2canvas(chartRef.current, { scale: 1 });
         const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF('landscape');
-        pdf.addImage(imgData, 'PNG', 10, 10, 280, 150);
+        const pdf = new jsPDF({
+            orientation: 'landscape',
+            unit: 'px',
+            format: [canvas.width, canvas.height]
+        });
+        pdf.addImage(imgData, 'PNG', 0, 0, canvas.width, canvas.height);
         pdf.save('chart.pdf');
     };
 
